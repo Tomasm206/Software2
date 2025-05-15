8ce7ddb4a488b2e5e659dff23cd46900c3fdc9a8
Tomás Marín <79349359+Tomasm206@users.noreply.github.com>
2025-05-15 16:32:04 -0500

    datos quemados v1.0

---
+++ Software2/src/main/java/com/example/aplication/outport/entity/AsistenciaEntity.java
@@ -0,0 +1,10 @@
+package com.example.aplication.outport.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "Asistencia")
+public class AsistenciaEntity {
+
+}
---
+++ Software2/src/main/java/com/example/aplication/outport/entity/SesionEntity.java
@@ -0,0 +1,5 @@
+package com.example.aplication.outport.entity;
+
+public class SesionEntity {
+
+}
